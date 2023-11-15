aee69126129e8289a414418e3576c6fdab332021
Jurabek Aminov <dzhurabek.aminov@mail.ru>
2023-11-15 23:17:25 +0300

    🔀 added organizations profiles API

---
+++ src/api/organizations.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends, status
+
+from src.schemas.organization import (
+    OrganizationRequestSchema,
+    OrganizationResponseSchema
+)
+from src.schemas.token import TokenPayloadSchema
+from src.services.organizations_service import OrganizationsService
+from src.utils.token import Token
+
+
+router = APIRouter(
+    prefix="/api/profiles/organizations",
+    tags=["Organization profiles"],
+)
+
+
+@router.post(
+    "",
+    response_model=OrganizationResponseSchema,
+    status_code=status.HTTP_201_CREATED
+)
+async def create_organization(
+    schema: OrganizationRequestSchema,
+    organization_info: TokenPayloadSchema = Depends(Token.get_organization_payload),
+    service: OrganizationsService = Depends()
+):
+    organization = await service.create_organization(int(organization_info.sub), schema)
+    return organization
+
+
+@router.get("", response_model=OrganizationResponseSchema)
+async def get_organization(
+    user_info: TokenPayloadSchema = Depends(Token.get_organization_viewer),
+    service: OrganizationsService = Depends()
+):
+    profile = await service.read_organization(user_id=user_info.org)
+    return profile
+
+
+@router.put("/me", status_code=status.HTTP_204_NO_CONTENT)
+async def update_organization(
+    schema: OrganizationRequestSchema,
+    organization_info: TokenPayloadSchema = Depends(Token.get_organization_payload),
+    service: OrganizationsService = Depends()
+):
+    await service.update_organization(organization_info.org, schema)
+
+
+@router.delete("/me", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_organization(
+    organization_info: TokenPayloadSchema = Depends(Token.get_organization_payload),
+    service: OrganizationsService = Depends()
+):
+    await service.delete_organization(organization_info.org)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from src.api.organizations import router as organizations_router
+
 
 app = FastAPI(
     title="Profiles service",
     description="Digital twin profiles microservice.",
     version="0.0.1"
 )
+
+app.include_router(organizations_router)
